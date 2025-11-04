f49f4c79894f2c88dba17105db2b55070531a6c8
CCB <chencheng20211010@163.com>
2025-11-04 18:36:27 +0800

    add

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ void sig_handler(int sig)
     errno = saveErrno;
 }
 
+
 // 添加信号
 void addsig(int sig, void(handler)(int), bool restart = true)
 {
@@ -61,7 +62,9 @@ int main(int argc, char** argv)
 {
     if(argc <= 2)
     {
-        cout << "usage: <ip> <port>" << endl;
+        #ifdef debug
+            cout << "usage: <ip> <port>" << endl;
+        #endif
         return 1;
     }
 
@@ -72,7 +75,7 @@ int main(int argc, char** argv)
     addsig(SIGPIPE, SIG_IGN);
 
     /* 创建线程池 */
-    std::shared_ptr<ThreadPool> threadsPool(new ThreadPool(4));
+    ThreadPool* threadsPool = new ThreadPool(4);
 
     /* 预先创建HTTP连接 */
     std::vector<HttpConn> users(MAX_FD);
@@ -92,17 +95,182 @@ int main(int argc, char** argv)
     assert(ret >= 0);
     ret = listen(listenFd, 8);
     assert(ret >= 0);
+
+    // 统一事件源
     epoll_event events[MAX_EVENT_NUMBER];
+    epollfd = epoll_create(5);
+    assert(epollfd >= 0);
+
+    addFd(epollfd, listenFd, false);
+    HttpConn::epollfd = epollfd;
+
+    // 创建管道
+    ret = socketpair(PF_UNIX, SOCK_STREAM, 0, pipefd);
+    assert(ret != -1);
+    setnoblocking(pipefd[1]);
+    addFd(epollfd, pipefd[0], false);
+
+    // 设置时钟，终止信号处理函数
+    addsig(SIGALRM, sig_handler, false);
+    addsig(SIGTERM, sig_handler, false);
+
+    bool stopServer = false;
+
+    bool timeout = false;
+
+    alarm(TIMESLOT);
+
+    int numbers = -1;
+    while(!stopServer)
+    {
+        numbers = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
+        if(numbers < 0 && errno != EINTR)
+        {
+            #ifdef debug
+                cout << "epoll_wait failed" << endl;
+            #endif
+            break;
+        }
+
+        for(int i = 0; i < numbers; ++i)
+        {
+            int sockfd = events[i].data.fd;
+
+            /* 说明有新连接 */
+            if(sockfd == listenFd)
+            {
+                sockaddr_in clntAddr;
+                socklen_t addrLen = 0;
+                
+                /* listenfd ET 模型*/
+                int connfd = -1;
+                while(1)
+                {
+                    connfd = accept(listenFd, (struct sockaddr*)&clntAddr, &addrLen);
+                    if(connfd < 0)
+                    {
+                        #ifdef debug
+                            cout << "accept error" << endl;
+                        #endif
+                        break;
+                    }
     
-
-   
+                    if(HttpConn::userCount >= MAX_FD)
+                    {
+                        #ifdef debug
+                            cout << "userCount >= MAX_FD" << endl;
+                        #endif
+                        break;
+                    }
+                    
+                    // 分配一个http并初始化连接
+                    users[connfd].init(connfd, clntAddr);
     
+                    // 设置定时器
+                    heapTimer.add(connfd, 3 * TIMESLOT, timer_handler);
+
+                }
+
+                continue;   // 不用继续if判断了
+            }
+            /* 关闭连接 */
+            else if(events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR))
+            {
+                // 删除定时器
+                heapTimer.doWork(sockfd);
+
+            }
+            /* 处理信号 */
+            else if((sockfd == pipefd[0]) && (events[i].events & EPOLLIN))
+            {
+                int sig = -1;
+                char signals[1024];
+                ret = recv(sockfd, signals, sizeof(signals), 0);
+                if(ret <= 0)
+                {
+                    continue;
+                }
+                else if(ret > 0)
+                {
+                    for(int i = 0; i < ret; ++i)
+                    {
+                        switch(signals[i])
+                        {
+                            case SIGALRM :
+                            {
+                                timeout = true;
+                                break;
+                            }
+                            case SIGTERM:
+                            {
+                                stopServer = true;
+                                break;
+                            }
+
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                /* 处理客户端连接上接到的数据 */
+                else if(events[i].events & EPOLLIN)
+                {
+                    if(users[sockfd].readFromClnt())
+                    {
+                        #ifdef debug
+                            cout << "deal with the client: " << inet_ntoa(users[sockfd].getAddr()->sin_addr);
+                        #endif
+
+                        // 线程池中加入任务
+                        threadsPool->addTask(users[sockfd].process);
+
+                        // 调整定时器
+                        heapTimer.adjust(sockfd, 3 * TIMESLOT);
+                    }
+                    else // 读失败
+                    {
+                        heapTimer.doWork(sockfd);
+                    }
+                }
+                /* 向客户端写数据 */
+                else if(events[i].events & EPOLLOUT)
+                {
+                    if(users[sockfd].writeToClnt())
+                    {
+                        #ifdef debug
+                        cout << "send data to the client" << inet_ntoa(users[sockfd].getAddr()->sin_addr) << endl;
+                        #endif
+
+                        // 调整定时器
+                        heapTimer.adjust(sockfd, 3 * TIMESLOT);
+                    }
+                    else
+                    {
+                        heapTimer.doWork(sockfd);
+                    }
+                }
 
 
+                // 出现超时信号了
+                if(timeout)
+                {
+                    timer_handler();
+                    timeout = false;
+                }
+
+            } 
+
+        }
+    }
 
 
-
-
-
+    close(epollfd);
+    close(listenFd);
+    close(pipefd[1]);
+    close(pipefd[0]);
+    delete threadsPool;
+    return 0;
+    
 }
 
